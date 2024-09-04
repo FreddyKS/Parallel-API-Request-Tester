da38335223a899834e0a3f118c8c9cda9669659a
FREDDY KURNIAWAN SOEBARKAH <135596061+FreddyKS@users.noreply.github.com>
2024-09-04 14:42:38 +0700

    Update api_throttling_test.js
    Supports adding body to non get method

--- Javascript/api_throttling_test.js
+++ Javascript/api_throttling_test.js
@@ -4,8 +4,8 @@ async function fetchMultipleUrls() {
     var url_list = document.getElementById('url_list').value.split("\n");
     var method = document.getElementById('method').value;
     var token = document.getElementById('bearer').value;
+    var postfield = document.getElementById('post_field').value;
     var content = document.getElementById('content_type').value;
-
     //Disable the button
     document.getElementById('print').disabled = true;
 
@@ -37,7 +37,40 @@ async function fetchMultipleUrls() {
         }
     }
     try {
-        // Run these requests in parallel using Promise.all
+        //If GET, don't send body
+        if(method=='GET'){
+            // Run these requests in parallel using Promise.all
+                const responses = await Promise.all(
+                    urls.map(url => 
+                        fetch(url, {
+                            method: `${method}`,
+                            headers: {
+                                'Authorization': `Bearer ${token}`,
+                                'Content-Type': `${content}`
+                            }
+                        })
+                    )
+                );
+            
+            // Parse all responses to JSON
+            const data = await Promise.all(responses.map(response => response.json()));
+            
+            // Display in file for further analysis
+            var data_str="";
+            data.forEach(item => {
+                document.getElementById('output').innerHTML += `<pre>${JSON.stringify(item, null, 2)}</pre>`;
+            });
+            
+            //Loading Complete
+            document.getElementById('complete').innerHTML='✔';
+            document.getElementById('loading').style.visibility='hidden';
+            
+        
+            //Re enable the button
+            document.getElementById('print').disabled = false;
+        }
+        else{
+            // Run these requests in parallel using Promise.all
             const responses = await Promise.all(
                 urls.map(url => 
                     fetch(url, {
@@ -45,28 +78,29 @@ async function fetchMultipleUrls() {
                         headers: {
                             'Authorization': `Bearer ${token}`,
                             'Content-Type': `${content}`
-                        }
+                        },
+                        body: JSON.stringify(postfield)
                     })
                 )
             );
-        
-        // Parse all responses to JSON
-        const data = await Promise.all(responses.map(response => response.json()));
-        
-        // Display in file for further analysis
-        var data_str="";
-        data.forEach(item => {
+
+            // Parse all responses to JSON
+            const data = await Promise.all(responses.map(response => response.json()));
+
+            // Display in file for further analysis
+            var data_str="";
+            data.forEach(item => {
             document.getElementById('output').innerHTML += `<pre>${JSON.stringify(item, null, 2)}</pre>`;
-        });
-        
-        //Loading Complete
-        document.getElementById('complete').innerHTML='✔';
-        document.getElementById('loading').style.visibility='hidden';
-        
-    
-        //Re enable the button
-        document.getElementById('print').disabled = false;
-    
+            });
+
+            //Loading Complete
+            document.getElementById('complete').innerHTML='✔';
+            document.getElementById('loading').style.visibility='hidden';
+
+
+            //Re enable the button
+            document.getElementById('print').disabled = false;
+        }
 
     } catch (error) {
         console.error('Error with fetching:', error);
