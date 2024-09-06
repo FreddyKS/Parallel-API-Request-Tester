034dbe3ecf9d8e04679727caace33890a9559e09
FREDDY KURNIAWAN SOEBARKAH <135596061+FreddyKS@users.noreply.github.com>
2024-09-06 17:31:09 +0700

    Installing composer for php

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'freddyks/parallel-api-request-tester',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '71703e633fae9e7617a921c198f8490f6686d9eb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'freddyks/parallel-api-request-tester' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '71703e633fae9e7617a921c198f8490f6686d9eb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
