64f9337dbd7efec716d3c7d1fa0c6c80c08e6c1c
FREDDY KURNIAWAN SOEBARKAH <135596061+FreddyKS@users.noreply.github.com>
2024-10-15 15:40:20 +0700

    Supports multilanguage

--- HTML + Javascript/api_throttling_test ORI.js
+++ HTML + Javascript/api_throttling_test ORI.js
@@ -1,42 +1,6 @@
 //Parallel api request is inspired by https://chatgpt.com/
-var list_data = "";
-function load_lang_variables(){
-    var lang = document.querySelector('input[name="language"]:checked').value;
-    if(lang=="EN"){
-        list_data = list_en;
-    }
-    else{
-        list_data = list_id;
-    }
-    document.getElementById('fetch-loading').style.visibility='hidden';
-}
-function load_language(){
 
-    //Load appropriate file
-    load_lang_variables();
-    
-    //Set text
-    document.getElementById("title_label").innerHTML = list_data.header_label;
-    document.getElementById("header_label").innerHTML = list_data.header_label;
-    document.getElementById("attention_label").innerHTML = list_data.attention_label;
-    document.getElementById("input_label").innerHTML = list_data.input_label;
-    document.getElementById("method_label").innerHTML = list_data.method_label;
-    document.getElementById("custom_method").placeholder = list_data.custom_method;
-    document.getElementById("url_list_label").innerHTML = list_data.url_list_label;
-    document.getElementById("post_field_label").innerHTML = list_data.post_field_label;
-    document.getElementById("loop_label").innerHTML = list_data.loop_label;
-    document.getElementById("danger_parallel_span").innerHTML = list_data.danger_parallel_span;
-    document.getElementById("bearer_label").innerHTML = list_data.bearer_label;
-    document.getElementById("submit").placeholder = list_data.submit;
-    document.getElementById("result_label").innerHTML = list_data.result_label;
-    document.getElementById("author_label").innerHTML = list_data.author_label;
-    changeColor();
-}
 async function fetchMultipleUrls() {
-    
-    //Load appropriate file
-    load_lang_variables();
-
     //Retrieve variables
     var loop = document.getElementById('loop').value;
     var url_list = document.getElementById('url_list').value.split("\n");
@@ -64,7 +28,7 @@ async function fetchMultipleUrls() {
         document.getElementById('fetch-loading').style.visibility='visible';
         document.getElementById('fetch-loading').style.backgroundColor='lightblue';
         document.getElementById('fetch-loading').style.color='white';
-        var display_loading = list_data.captcha_please;
+        var display_loading = 'Please insert Captcha code';
         document.getElementById('complete').innerHTML=display_loading;
         //Re enable the button on error
         document.getElementById('print').disabled = false;
@@ -74,7 +38,7 @@ async function fetchMultipleUrls() {
         document.getElementById('fetch-loading').style.visibility='visible';
         document.getElementById('fetch-loading').style.backgroundColor='red';
         document.getElementById('fetch-loading').style.color='white';
-        var display_loading = list_data.wrong_captcha;
+        var display_loading = 'Wrong Captcha';
         document.getElementById('complete').innerHTML=display_loading;
         //Re enable the button on error
         document.getElementById('print').disabled = false;
@@ -107,7 +71,7 @@ async function fetchMultipleUrls() {
         document.getElementById('fetch-loading').style.visibility='visible';
         document.getElementById('fetch-loading').style.backgroundColor='red';
         document.getElementById('fetch-loading').style.color='white';
-        var display_loading = '`'+list_data.url_list_label+'`'+list_data.api_cannot_empty;
+        var display_loading = '`API list` cannot be empty';
         document.getElementById('complete').innerHTML=display_loading;
         //Re enable the button on error
         document.getElementById('print').disabled = false;
@@ -118,7 +82,7 @@ async function fetchMultipleUrls() {
             document.getElementById('fetch-loading').style.visibility='visible';
             document.getElementById('fetch-loading').style.backgroundColor='red';
             document.getElementById('fetch-loading').style.color='white';
-            var display_loading = list_data.bearer_same_as_token_not_empty+'`'+list_data.bearer_label+'`'+ list_data.line_same_as+'`'+list_data.url_list_label+'`';
+            var display_loading = 'TOKEN NOT EMPTY\n `Bearer` lines have to be the same as `API list`';
             document.getElementById('complete').innerHTML=display_loading;
             //Re enable the button on error
             document.getElementById('print').disabled = false;
@@ -130,7 +94,7 @@ async function fetchMultipleUrls() {
             document.getElementById('fetch-loading').style.visibility='visible';
             document.getElementById('fetch-loading').style.backgroundColor='red';
             document.getElementById('fetch-loading').style.color='white';
-            var display_loading = list_data.multiple_postfield_not_empty+'`POST FIELD`'+list_data.line_same_as+'`'+list_data.url_list_label+'`';
+            var display_loading = 'MULTIPLE POSTFIELD NOT EMPTY\n `POST FIELD` lines have to be the same as `API list`';
             document.getElementById('complete').innerHTML=display_loading;
             //Re enable the button on error
             document.getElementById('print').disabled = false;
@@ -144,11 +108,9 @@ async function fetchMultipleUrls() {
     }
     //Loading starts
     let startTime = new Date();
-    var display_loading = list_data.fetching + parseInt(loop)*parseInt(url_list.length) + list_data.request;
+    var display_loading = 'Fetching ' + parseInt(loop)*parseInt(url_list.length) + ' request';
     document.getElementById('complete').innerHTML=display_loading;
     document.getElementById('loading').style.display='block';
-    document.getElementById('loading').style.display='block';
-    document.querySelector('input[name="language"]').disabled=true;
     
     const urls = [];
 
@@ -222,7 +184,7 @@ async function fetchMultipleUrls() {
                         }
                     }
                     else{
-                        return list_data.response_unavailable;
+                        return "(Response from Parallel-API-Request-Tester) => Response unavailable";
                     }
                 })
             );
@@ -236,9 +198,9 @@ async function fetchMultipleUrls() {
             //Loading Complete
             let endTime = new Date();
             let timeElapsed = (endTime - startTime)/1000;
-            document.getElementById('complete').innerHTML=loop+list_data.parallel+method+list_data._request+'`'+document.getElementById('url_list').value+list_data.done_in+timeElapsed +list_data.seconds;
+            document.getElementById('complete').innerHTML=loop+' parallel `'+method+'` request on '+'`'+document.getElementById('url_list').value+'` done in <br>'+timeElapsed + ' seconds';
             document.getElementById('loading').style.display='none';
-            document.querySelector('input[name="language"]').disabled=false;
+            
         
             //Re enable the button
             document.getElementById('print').disabled = false;
@@ -278,7 +240,7 @@ async function fetchMultipleUrls() {
                         }
                     }
                     else{
-                        return list_data.response_unavailable;
+                        return "(Response from Parallel-API-Request-Tester) => Response unavailable";
                     }
                 })
             );
@@ -292,7 +254,7 @@ async function fetchMultipleUrls() {
             //Loading Complete
             let endTime = new Date();
             let timeElapsed = (endTime - startTime)/1000;
-            document.getElementById('complete').innerHTML='`'+document.getElementById('url_list').value+list_data.done_in+timeElapsed + list_data.seconds;
+            document.getElementById('complete').innerHTML='`'+document.getElementById('url_list').value+'` done in <br>'+timeElapsed + ' seconds';
             document.getElementById('loading').style.display='none';
 
 
@@ -305,7 +267,7 @@ async function fetchMultipleUrls() {
         document.getElementById('fetch-loading').style.backgroundColor='red';
         document.getElementById('fetch-loading').style.color='white';
         document.getElementById('loading').style.display='none';
-        document.getElementById('complete').innerHTML=list_data.cannot_access+document.getElementById('url_list').value+list_data.with_method+method+list_data.error_detail+error+'`';
+        document.getElementById('complete').innerHTML='Cannot access `'+document.getElementById('url_list').value+'` with Method `'+method+'`<br>Error Detail -> `'+error+'`';
         //Re enable the button on error
         document.getElementById('print').disabled = false;
         return;
@@ -313,65 +275,58 @@ async function fetchMultipleUrls() {
 }
 
 function changeColor(){
-
-    //Load language
-    load_lang_variables();
     var method = document.getElementById('method').value;
     var api_list = document.getElementById('url_list').value;
     document.getElementById('print').className='button';
     if(method=='GET'){
         document.getElementById('method').className='getmethod';
         document.getElementById('print').classList.add('getmethod');
-        document.querySelector('#print').innerHTML = list_data.test+' GET '+api_list;
+        document.querySelector('#print').innerHTML = 'Test GET '+api_list;
     }
     else if(method=='POST'){
         document.getElementById('method').className='postmethod';
         document.getElementById('print').classList.add('postmethod');
-        document.querySelector('#print').innerHTML = list_data.test+' POST '+api_list;
+        document.querySelector('#print').innerHTML = 'Test POST '+api_list;
     }
     else if(method=='PUT' || method=='PATCH'){
         document.getElementById('method').className='updatemethod';
         document.getElementById('print').classList.add('updatemethod');
         if(method=='PUT'){
-            document.querySelector('#print').innerHTML = list_data.test+' PUT '+api_list;
+            document.querySelector('#print').innerHTML = 'Test PUT '+api_list;
         }
         else{
-            document.querySelector('#print').innerHTML = list_data.test+' PATCH '+api_list;
+            document.querySelector('#print').innerHTML = 'Test PATCH '+api_list;
         }
     }    
     else if(method=='DELETE'){
         document.getElementById('method').className='deletemethod';
         document.getElementById('print').classList.add('deletemethod');
-        document.querySelector('#print').innerHTML = list_data.test+' DELETE '+api_list;
+        document.querySelector('#print').innerHTML = 'Test DELETE '+api_list;
     }
     else if(method=='HEAD'){
         document.getElementById('method').className='headmethod';
         document.getElementById('print').classList.add('headmethod');
-        document.querySelector('#print').innerHTML = list_data.test+' HEAD '+api_list;
+        document.querySelector('#print').innerHTML = 'Test HEAD '+api_list;
     }
     else if(method=='OPTIONS'){
         document.getElementById('method').className='optionsmethod';
         document.getElementById('print').classList.add('optionsmethod');
-        document.querySelector('#print').innerHTML = list_data.test+' OPTIONS '+api_list;
+        document.querySelector('#print').innerHTML = 'Test OPTONS '+api_list;
     }
     else if(method=='DELETE'){
         document.getElementById('method').className='deletemethod';
         document.getElementById('print').classList.add('deletemethod');
-        document.querySelector('#print').innerHTML = list_data.test+' DELETE '+api_list;
+        document.querySelector('#print').innerHTML = 'Test DELETE '+api_list;
     }
     else{
         document.getElementById('method').className='othermethod';
         document.getElementById('print').classList.add('othermethod');
-        document.querySelector('#print').innerHTML = list_data.test+' '+method+' '+api_list;
+        document.querySelector('#print').innerHTML = 'Test '+method+' '+api_list;
     }
 }
 
 //If user input custom method
 function handleCustomMethod(){
-    
-    //Load appropriate file
-    load_lang_variables();
-    
     var method_value = document.getElementById('method').value;
     var method_innerHTML = document.getElementById('method').innerHTML;
     var api_list = document.getElementById('url_list').value;
@@ -382,14 +337,14 @@ function handleCustomMethod(){
         document.getElementById('print').className='button';
         var method_class_name = document.getElementById('method').className;
         document.getElementById('print').classList.add(method_class_name);
-        document.querySelector('#print').innerHTML = list_data.test+' '+method_value+' '+api_list;
+        document.querySelector('#print').innerHTML = 'Test '+method_value+' '+api_list;
     }
     else{
         document.getElementById('method').disabled=true;
         document.getElementById('custom_method').className='othermethod';
         document.getElementById('custom_method').classList.add('othermethod');
         document.getElementById('print').classList.add('othermethod');
-        document.querySelector('#print').innerHTML = list_data.test+' '+custom_method+' '+api_list;
+        document.querySelector('#print').innerHTML = 'Test '+custom_method+' '+api_list;
     }
 }
 
@@ -405,9 +360,6 @@ function displayDanger(){
 
 //Generate Captcha
 function generate() {
-
-    //Load appropriate file
-    //load_lang_variables();
     // Clear old input
     document.getElementById("submit").value = "";
 
---
+++ HTML + Javascript/lang-list.js
@@ -0,0 +1,119 @@
+/* 
+    Define your own language wording
+    Copy, edit the following template, then paste this on the lowest line 
+    don't replace the following text
+    <b> : <br>, </b>, \n and non empty quoted strings
+    See example below
+    
+    //<your language> word mapping
+
+    list_<your language> = {
+    //In html wordings
+    "header_label":"",
+    "attention_label":"<b> : <br><br> </b>",
+    "input_label":"",
+    "method_label":"",
+    "custom_method":"",
+    "url_list_label":"",
+    "post_field_label":"POST / PUT / PATCH (POSTFIELD)",
+    "loop_label":"",
+    "danger_parallel_span":"",
+    "bearer_label":"Bearer",
+    "submit":"",
+    "result_label":"",
+    "author_label":"",
+    
+    //In javascript wordings
+    "api_cannot_empty":"",
+    "line_same_as":"",
+    "bearer_same_as_token_not_empty":"\n",
+    "multiple_postfield_not_empty":"\n",
+    "fetching":" ",
+    "request":" ",
+    "parallel":"  `",
+    "_request":"`  ",
+    "done_in":"`  <br>",
+    "seconds":"",
+    "cannot_access":" `",
+    "with_method":"`  `",
+    "error_detail":"`<br> -> `",
+    "test":"",
+    "wrong_captcha":"",
+    "captcha_please":"",
+    "response_unavailable":"() => "
+    };
+*/
+
+//English word mapping
+list_en = {
+    //In html wordings
+    "header_label":"Parallel API Request Tester",
+    "attention_label":"<b>ATTENTION : <br>BY USING THIS TOOL, YOU AGREED 100% NOT TO BE INVOLVED IN ANY CYBER ATTACK.<br> THE DEVELOPER HELD NO RESPONSIBILITY FOR ANYTHING</b>",
+    "input_label":"Input",
+    "method_label":"Method",
+    "custom_method":"Or enter your own method",
+    "url_list_label":"API list",
+    "post_field_label":"POST / PUT / PATCH field (POSTFIELD)",
+    "loop_label":"How many parallel",
+    "danger_parallel_span":"WARNING : MORE THAN 50 PARALLEL REQUESTS. THE DEVELOPER HELD NO RESPONSIBILITY FOR ANYTHING",
+    "bearer_label":"Bearer",
+    "submit":"Captcha code",
+    "result_label":"Result",
+    "author_label":"Author : freddyksoe76712@gmail.com",
+    
+    //In javascript wordings
+    "api_cannot_empty":"cannot be empty",
+    "line_same_as":" lines have to be the same as ",
+    "bearer_same_as_token_not_empty":"BEARER ERROR\n ",
+    "multiple_postfield_not_empty":"POSTFIELD ERROR\n ",
+    "fetching":"Fetching ",
+    "request":" request",
+    "parallel":" parallel `",
+    "_request":"` request on ",
+    "done_in":"` done in <br>",
+    "seconds":" seconds",
+    "cannot_access":"Cannot access `",
+    "with_method":"` with Method `",
+    "error_detail":"`<br>Error Detail -> `",
+    "test":"Test",
+    "wrong_captcha":"Wrong Captcha",
+    "captcha_please":"Please insert Captcha code",
+    "response_unavailable":"(Response from Parallel-API-Request-Tester) => Response unavailable"
+};
+
+//Indonesian word mapping
+list_id = {
+    //In html wordings
+    "header_label":"Tester Request API Paralel",
+    "attention_label":"<b>PERHATIAN : <br>DENGAN MEMAKAI ALAT INI, ANDA SETUJU 100% TIDAK AKAN TERLIBAT DALAM SERANGAN CYBER.<br> PENGEMBANG TIDAK BERTANGGUNG JAWAB ATAS APAPUN</b>",
+    "input_label":"Input",
+    "method_label":"Metode",
+    "custom_method":"Atau masukkan metode anda sendiri",
+    "url_list_label":"List API",
+    "post_field_label":"POST / PUT / PATCH (POSTFIELD)",
+    "loop_label":"Berapa banyak paralel",
+    "danger_parallel_span":"HATI-HATI : LEBIH DARI 50 REQUEST PARALEL, PENGEMBANG TIDAK BERTANGGUNG JAWAB ATAS APAPUN",
+    "bearer_label":"Bearer",
+    "submit":"Kode Captcha",
+    "result_label":"Hasil",
+    "author_label":"Pengembang : freddyksoe76712@gmail.com",
+    
+    //In javascript wordings
+    "api_cannot_empty":"tidak bisa kosong",
+    "line_same_as":" harus sama panjangnya dengan ",
+    "bearer_same_as_token_not_empty":"EROR BEARER\n ",
+    "multiple_postfield_not_empty":"EROR POSTFIELD\n ",
+    "fetching":"Memproses ",
+    "request":" request",
+    "parallel":" paralel `",
+    "_request":"` request pada ",
+    "done_in":"` selesai dalam <br>",
+    "seconds":" detik",
+    "cannot_access":"Tidak bisa mengakses `",
+    "with_method":"` dengan Metode `",
+    "error_detail":"`<br>Detail Error -> `",
+    "test":"Tes",
+    "wrong_captcha":"Salah Captcha",
+    "captcha_please":"Mohon masukkan Kode Captcha",
+    "response_unavailable":"(Response dari Tester Request API Paralel) => Response tidak ada"
+};
